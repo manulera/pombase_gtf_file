3effba3313aeae07698c6bdf511a52ab93676873
Manuel Lera Ramirez <manulera14@gmail.com>
2023-08-03 15:57:03 +0200

    now python script is ran inside container

--- run_with_docker.sh
+++ run_with_docker.sh
@@ -5,6 +5,6 @@ bash get_data.sh
 docker pull quay.io/biocontainers/bioconvert:1.1.1--pyhdfd78af_0
 
 # Run file conversion using docker
-docker run --rm -v $PWD/scripts:/scripts:ro -v $PWD/data:/data quay.io/biocontainers/bioconvert:1.1.1--pyhdfd78af_0\
-        bioconvert gff32gtf --force data/pombe_genome.gff3 data/pombe_genome_unprocessed.gtf;
-        python scripts/pombasegtf_to_ensemblgtf.py --input_file data/pombe_genome_unprocessed.gtf --output_file data/pombe_genome.gtf --gene_types data/gene_IDs_names_products.tsv
+docker run --rm -v $PWD/scripts:/scripts:ro -v $PWD/data:/data quay.io/biocontainers/bioconvert:1.1.1--pyhdfd78af_0 /bin/bash -c \
+        "bioconvert gff32gtf --force data/pombe_genome.gff3 data/pombe_genome_unprocessed.gtf;\
+        python scripts/pombasegtf_to_ensemblgtf.py --input_file data/pombe_genome_unprocessed.gtf --output_file data/pombe_genome.gtf --gene_types data/gene_IDs_names_products.tsv"
